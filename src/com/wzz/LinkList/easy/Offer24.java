26ff3e3fb4e67e225652a1ea3a5d7b9fc31a26ab
wzz1206414629 <1206414629@qq.com>
2020-09-27 19:49:37 +0800

    Day 09/27

---
+++ src/com/wzz/LinkList/easy/Offer24.java
@@ -0,0 +1,27 @@
+package com.wzz.LinkList.easy;
+
+/*
+定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+
+示例:
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+
+限制：
+0 <= 节点个数 <= 5000
+ */
+public class Offer24 {
+    public ListNode reverseList(ListNode head) {
+        //保存上一个节点
+        ListNode prev = null;
+        //当前连标的节点
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode nextTemp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = nextTemp;
+        }
+        return prev;
+    }
+}
