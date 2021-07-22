113eceb919ae4888d835b8dddafd59a8961f62a3
wzz1206414629 <1206414629@qq.com>
2021-07-22 21:07:35 +0800

    day 2021/07/22

---
+++ src/com/wzz/leetcode/Array/easy/L278.java
@@ -0,0 +1,48 @@
+package com.wzz.leetcode.Array.easy;
+
+/**
+ * @Date 2021/7/22 20:11
+ * @created by wzz
+ * 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+ * <p>
+ * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+ * <p>
+ * 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+ * <p>
+ *  
+ * 示例 1：
+ * <p>
+ * 输入：n = 5, bad = 4
+ * 输出：4
+ * 解释：
+ * 调用 isBadVersion(3) -> false
+ * 调用 isBadVersion(5) -> true
+ * 调用 isBadVersion(4) -> true
+ * 所以，4 是第一个错误的版本。
+ * 示例 2：
+ * <p>
+ * 输入：n = 1, bad = 1
+ * 输出：1
+ * <p>
+ * 提示：
+ * 1 <= bad <= n <= 231 - 1
+ */
+public class L278 {
+    public int firstBadVersion(int n) {
+        int start = 1, end = n;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    // leetcode系统提供,这里写只是为了编译不报错
+    private boolean isBadVersion(int mid) {
+        return false;
+    }
+}
---
+++ src/com/wzz/leetcode/Array/easy/L704.java
@@ -0,0 +1,43 @@
+package com.wzz.leetcode.Array.easy;
+
+/**
+ * @Date 2021/7/22 19:38
+ * @created by wzz
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [-1,0,3,5,9,12], target = 9
+ * 输出: 4
+ * 解释: 9 出现在 nums 中并且下标为 4
+ * 示例 2:
+ * <p>
+ * 输入: nums = [-1,0,3,5,9,12], target = 2
+ * 输出: -1
+ * 解释: 2 不存在 nums 中因此返回 -1
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 你可以假设 nums 中的所有元素是不重复的。
+ * n 将在 [1, 10000]之间。
+ * nums 的每个元素都将在 [-9999, 9999]之间。
+ */
+public class L704 {
+
+    public int search(int[] nums, int target) {
+        int mid, start = 0, end = nums.length - 1;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
