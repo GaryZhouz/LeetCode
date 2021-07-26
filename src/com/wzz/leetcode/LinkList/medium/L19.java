93b4d2ded5a7e901224c5301a2c7845f1d7d4b43
wzz1206414629 <1206414629@qq.com>
2021-07-26 21:06:11 +0800

    day 2021/07/26

---
+++ src/com/wzz/leetcode/LinkList/medium/L19.java
@@ -0,0 +1,52 @@
+package com.wzz.leetcode.LinkList.medium;
+
+import com.wzz.leetcode.LinkList.ListNode;
+
+/**
+ * @Date 2021/7/26 19:53
+ * @created by wzz
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ * <p>
+ * 进阶：你能尝试使用一趟扫描实现吗？
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5], n = 2
+ * 输出：[1,2,3,5]
+ * 示例 2：
+ * <p>
+ * 输入：head = [1], n = 1
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：head = [1,2], n = 1
+ * 输出：[1]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 链表中结点的数目为 sz
+ * 1 <= sz <= 30
+ * 0 <= Node.val <= 100
+ * 1 <= n <= sz
+ */
+public class L19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode listNode = new ListNode(0);
+        listNode.next = head;
+        ListNode fast = listNode;
+        ListNode slow = listNode;
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return listNode.next;
+    }
+}
